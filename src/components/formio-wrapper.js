ae458ae6bb1f4def3d5edad5efb10dc971bfee7e
Jordan Binskin <jordan.binskin@gmail.com>
2020-12-14 15:45:01 +1100

    remove next handlers

--- src/components/formio-wrapper.js
+++ src/components/formio-wrapper.js
@@ -73,9 +73,6 @@ export class FormioWrapper {
 
     baseObject.addEventListener('formiowrapperGoToNext', () => {
       this._goToNextPage();
-      if (this.config.extraTriggersOnActions.next) {
-        this._fireExtraEvent(this.config.extraTriggersOnActions.next);
-      }
     });
 
     baseObject.addEventListener('formiowrapperGoToPrevious', () => {
